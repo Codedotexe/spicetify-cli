83e16d0974ce54c5a8daa393245b69e7b659d7cb
Tom <36423219+Codedotexe@users.noreply.github.com>
2023-03-17 17:21:31 +0100

    Update genresInfo.js
    Remove genresInfo for albums. Although this field exists in the spotify api it seems to always be empty. Genres info for artists although is still supported.

--- Extensions/genresInfo.js
+++ Extensions/genresInfo.js
@@ -8,54 +8,36 @@
 
 (function genresInfo() {
 	const { CosmosAsync, ContextMenu, URI } = Spicetify;
+
 	if (!(CosmosAsync && URI)) {
 		setTimeout(genresInfo, 300);
 		return;
 	}
 
 	async function showGenres(uris) {
-		var request = new XMLHttpRequest();
+		const request = new XMLHttpRequest();
 		const uriObj = Spicetify.URI.fromString(uris[0]);
 		const id = uris[0].split(":")[2];
 
-		let res;
 		if (uriObj.type === Spicetify.URI.Type.ARTIST) {
-			res = await CosmosAsync.get("https://api.spotify.com/v1/artists/" + id);
-		} else if (uriObj.type === Spicetify.URI.Type.ALBUM) {
-			res = await CosmosAsync.get("https://api.spotify.com/v1/albums/" + id);
-		} else {
-			return; // Not an artist or album (should never happen)
-		}
+			const res = await CosmosAsync.get("https://api.spotify.com/v1/artists/" + id);
 
-		Spicetify.PopupModal.display({
-			title: "Genres",
-			content: res.genres.length > 0 ? `Genres: ${res.genres.join(", ")}` : "No genres found",
-		});
+			Spicetify.PopupModal.display({
+				title: "Genres",
+				content: res.genres.length > 0 ? `Genres: ${res.genres.join(", ")}` : "No genres found",
+			});
+		}
 	}
 
-	function isUriThisType(uris, uriType) {
+	function isArtistURI(uris) {
 		if (uris.length > 1) {
 			return false;
 		}
 		const uri = uris[0];
 		const uriObj = Spicetify.URI.fromString(uri);
-		if (uriObj.type === uriType) {
-			return true;
-		}
-		return false;
-	}
-
-	function isArtistURI(uris) {
-		return isUriThisType(uris, Spicetify.URI.Type.ARTIST);
-	}
-
-	function isAlbumURI(uris) {
-		return isUriThisType(uris, Spicetify.URI.Type.ALBUM);
+		return uriObj.type === Spicetify.URI.Type.ARTIST
 	}
 
 	const artistGenresContextMenu = new Spicetify.ContextMenu.Item("Show genres", showGenres, isArtistURI);
 	artistGenresContextMenu.register();
-
-	const albumGenresContextMenu = new Spicetify.ContextMenu.Item("Show genres", showGenres, isAlbumURI);
-	albumGenresContextMenu.register();
 })();
